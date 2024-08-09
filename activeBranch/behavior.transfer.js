acd82acb40515a5fdd6f12ec17b173d92ed26c98
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2024-08-09 09:17:33 -0400

    Update behavior.transfer.js
    + comments
    + looting rules
    + memory correction

--- activeBranch/behavior.transfer.js
+++ activeBranch/behavior.transfer.js
@@ -2,37 +2,35 @@ if(!Creep.prototype.ExecuteTransferCommand)
 {
     Creep.prototype.ExecuteTransferCommand = function()
     {
-        
         var targetObject = Game.getObjectById(this.memory.command.targetID);
         var targetRoom = Game.rooms[this.memory.command.roomName];
 
         if(this.memory.command.isDelivery)
         {
             
-            if(this.store.getFreeCapacity() == 0 && !this.memory.command.delivering)
+            if(!this.memory.command.delivering && this.store.getFreeCapacity() == 0)
             {
                 this.memory.command.delivering = true;
             }
             
-            if(this.memory.entireJob)
+            if(this.memory.command.entireJob)// If instructed to fill the entire container (this is dangerous and really should only be run on turrets, extensions, spawns, etc.)...
             {
-                if(this.memory.command.delivering)
+                if(this.memory.command.delivering) // ...and we've collected resources and are delivering...
                 {
-                    if(targetObject.store.getFreeCapacity() == 0 )
+                    if(targetObject.store.getFreeCapacity() == 0 ) // ...and the object is full (for any reason)...
                     {
                         //TODO: need to find a way to report completion on these commands... perhaps a status field in the default command?
                         //May need to be an added function in proto.mod. Admittedly would be easier if I decoupled commands from spawns.
-                        //Hey! If commands are decoupled, creeps wouldn't need their own memory copy! All they'd need is the ID of their active command.
                         //It'd have to be reported by commander, and likely through main? Or again, via the creep proto. Meh, good enough.
                         delete this.memory.command;
                     } 
-                    else if(this.store.getUsedCapacity() == 0)
+                    else if(this.store.getUsedCapacity() == 0) // ...and we're out of resources...
                     {
-                        this.memory.delivering = false;
+                        this.memory.command.delivering = false;
                     }
                 }
             }
-            else if(this.store.getUsedCapacity() == 0 && this.memory.command.delivering)
+            else if(this.store.getUsedCapacity() == 0 && this.memory.command.delivering) //If instructed to only make one delivery run, and we're empty after having gathered resources...
             {
                 delete this.memory.command;
             }
@@ -40,7 +38,22 @@ if(!Creep.prototype.ExecuteTransferCommand)
         
         if(!this.memory.command.isDelivery)
         {
-            if(!this.memory.haveLooted)
+            if(this.memory.command.entireJob)
+            {
+                // If we're still looting and realize we are full, the object is gone, or the object is empty...
+                if(!this.memory.command.returningResource && (this.store.getFreeCapacity() == 0 || !targetObject || targetObject.store.getUsedCapacity() == 0))
+                {
+                    this.memory.command.returningResource = true; //Return resources.
+                }
+
+                // If while returning resources we see we are empty, check to see if the object is gone or empty. If so, consider command complete.
+                if((this.memory.command.returningResource && this.store.getUsedCapacity() == 0) && (!targetObject || targetObject.store.getUsedCapacity() == 0))
+                {
+                    delete this.memory.command;
+                }
+            }
+
+            if(!this.memory.returningResource)
             {
                 if(this.pickup(targetObject) != OK && this.withdraw(targetObject) != OK)
                 {
@@ -50,3 +63,5 @@ if(!Creep.prototype.ExecuteTransferCommand)
         }
     }
 }
+
+//Seeing all the diferent end-cases, might consider making a Terminate...Command() to clean up creep memory.
