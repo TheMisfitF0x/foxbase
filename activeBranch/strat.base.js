dfadc0eccc79b76f32291cf20d7217f6cfad9932
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2024-09-13 11:37:15 -0400

    Write base strategy class
    Cry. It's bad.

---
+++ activeBranch/strat.base.js
@@ -0,0 +1,15 @@
+//Oh no not another class.
+class Strategy
+{
+    constructor()
+    {
+
+    }
+
+    Action()
+    {
+        console.log("No action associated with this state. Created base state object. Bad.");
+    }
+}
+
+module.exports = Strategy;
